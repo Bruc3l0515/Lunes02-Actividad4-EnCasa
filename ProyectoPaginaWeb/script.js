b24fbc1c8aa30ddc0bb953c9cd53771bd15f776e
Facundo Chacón <chaconfacu1984@gmail.com>
2024-12-02 10:57:55 -0300

    Add files via upload

--- ProyectoPaginaWeb/script.js
+++ ProyectoPaginaWeb/script.js
@@ -1,3 +1,7 @@
 let nombre = 'Facundo';
 const edad = 18;
-console.log('Hola, me llamo ${nombre} y tengo ${edad} años')
+console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`)
+let suma = 10 + 4
+console.log(`La suma de 10+4 es: ${suma}`)
+let multiplicacion = 3 * 4
+console.log(`La multiplicacion entre 3x4 es: ${multiplicacion}`)
