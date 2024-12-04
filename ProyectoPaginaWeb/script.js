1a102c3d4a34646e43673d9e286b6ff308993a82
Bruc3l0515 <chaconfacu1984@gmail.com>
2024-12-03 21:30:19 -0300

    Se ha añadido mas codigo en la carpeta JS

--- ProyectoPaginaWeb/script.js
+++ ProyectoPaginaWeb/script.js
@@ -1,7 +1,31 @@
 let nombre = 'Facundo';
 const edad = 18;
-console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`)
-let suma = 10 + 4
-console.log(`La suma de 10+4 es: ${suma}`)
-let multiplicacion = 3 * 4
-console.log(`La multiplicacion entre 3x4 es: ${multiplicacion}`)
+console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`);
+let suma = 10 + 4;
+console.log(`La suma de 10+4 es: ${suma}`);
+let multiplicacion = 3 * 4;
+console.log(`La multiplicacion entre 3x4 es: ${multiplicacion}`);
+let nombre2 = 'Francisco';
+nombre2 = 'Pablo';
+console.log(`Mi nombre es ${nombre2}`);
+const nombre3 = 'Lorenzo';
+console.log(`Mi nombre es ${nombre3}`);
+let edad2 = 20;
+if(edad2 >= 18 ) {
+    console.log(`Eres mayor de edad`);
+}    else {
+    console.log(`Eres menor de edad`)
+}
+
+
+
+
+
+
+
+
+/*Este es un comentario donde explico el codigo, y el codigo
+  tiene la funcion de experimentar y poder ver los principios
+  basicos de JS*/
+
+
