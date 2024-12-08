4484295fff70f8675ebb4974f9a94b2f44ef42d0
Bruc3l0515 <chaconfacu1984@gmail.com>
2024-12-08 18:50:10 -0300

    Se ha añadido mas codigo en los archivos JS, CSS y se ha añadido un nuevo archivo html llamado calculadora

--- ProyectoPaginaWeb/script.js
+++ ProyectoPaginaWeb/script.js
@@ -1,21 +1,35 @@
 let nombre = 'Facundo';
-const edad = 18;
-console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`);
+console.log(`Hola, me llamo ${nombre}`);
 let suma = 10 + 4;
 console.log(`La suma de 10+4 es: ${suma}`);
-let multiplicacion = 3 * 4;
-console.log(`La multiplicacion entre 3x4 es: ${multiplicacion}`);
+let mayor = 10;
+console.log(mayor > 5 && "Es mayor a 5");
+let menor = 5;
+console.log(menor < 2 && "Es menor a 2");
 let nombre2 = 'Francisco';
 nombre2 = 'Pablo';
 console.log(`Mi nombre es ${nombre2}`);
 const nombre3 = 'Lorenzo';
 console.log(`Mi nombre es ${nombre3}`);
-let edad2 = 20;
-if(edad2 >= 18 ) {
-    console.log(`Eres mayor de edad`);
-}    else {
-    console.log(`Eres menor de edad`)
-}
+let mayorDeEdad = 20;
+console.log(mayorDeEdad > 18 && "Es mayor de edad");
+let nombre4 = "carlos";
+console.log(typeof nombre4);
+const animales = ["chancho", "vaca", "gallina", "oveja", "cordero", "mula", "guanaco", "burro", "caballo"];
+let animal = animales[3];
+console.log(animal);
+let resto = 7 % 3;
+console.log(`El resto de la division entre 7 y 3 es: ${resto}`)
+let nada = null;
+console.log(nada);
+const cuenta = [suma2, resta, multiplicacion, division];
+const nums = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "cero"];
+let suma2 = nums + nums;
+
+
+
+
+
 
 
 
